4f187194586e743b86d7a1e23284ef643e99ae47
Scott Haley <scott.haley@dialexa.com>
2022-01-15 11:37:32 -0600

    Add auth0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AuthModule } from '@auth0/auth0-angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,6 +13,10 @@ import { HomeModule } from './modules/home/home.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    AuthModule.forRoot({
+      domain: 'dev-fe7bancg.us.auth0.com',
+      clientId: 'mUQi9ksENWXm1qqwzwChJ1J7VFxT5y4f'
+    }),
     HomeModule
   ],
   providers: [],
